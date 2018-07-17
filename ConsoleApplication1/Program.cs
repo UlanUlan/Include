e1b328768a0a77dce1d70cb5c9882c79f9f141f6
ЖолдасхановУ <@242_13>
2018-07-17 21:51:43 +0600

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,64 @@
+﻿using ConsoleApplication1.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        //private static MCSEntities db = new MCSEntities();
+        static void Main(string[] args)
+        {
+            MCSEntities db = new MCSEntities();
+            db.Database.Log = Console.Write;
+
+
+            //явная загрузка
+
+            TablesModel model = db.TablesModels.Find(1);
+
+            foreach (var source in db.TablesSNPrefixes.Where(w=>w.intModelID==model.intModelID))
+            {
+                model.TablesSNPrefixes.Add(source);
+            }
+
+            db.Entry(model).Collection(c => c.TablesSNPrefixes).Load();
+
+
+            return;
+
+            //Ленивая (отложенная) загрузка
+
+            foreach (var tablesModel in db.TablesModels.Take(5))
+            {
+                Console.WriteLine(tablesModel.strName);
+                foreach (var tablesSnPrefix in tablesModel.TablesSNPrefixes)
+                {
+                    Console.WriteLine("   -" + tablesSnPrefix.strPrefix);
+                }
+            }
+
+            return;
+
+            //Прямая загрузка
+
+            List<newEquipment> equipments = db.newEquipments.Include(d => d.TablesLocation).Include(d => d.TablesManufacturer).Include(d => d.TablesModel.TablesSNPrefixes).ToList();
+
+            Console.WriteLine("-----------------------------------");
+
+            foreach (var newEquipment in equipments)
+            {
+                Console.WriteLine(newEquipment.intGarageRoom);
+                foreach (var pref in newEquipment.TablesModel.TablesSNPrefixes)
+                {
+                    Console.WriteLine("->" + pref.strPrefix);
+                }
+            }
+            Console.WriteLine("--------------------------------------------");
+        }
+    }
+}
---
+++ Entity/Program.cs
@@ -0,0 +1,71 @@
+﻿using Entity.NewFolder1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Entity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exmpl02();
+        }
+        public static void Exmpl01()
+        {
+            mcs db = new mcs();
+
+            db.Database.Log = (s => System.Diagnostics.Debug.WriteLine(s));
+
+            int i = 0;
+
+            foreach (var accesTab in db.AccessTab)
+            {
+                i++;
+                Console.WriteLine(accesTab.strTabName);
+                foreach (var item in accesTab.AccessUser)
+                {
+                    Console.WriteLine("\t--> "+item.intUserId);i++;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(i);
+
+           // List<AccessTab> tabs = db.AccessTab.Include(c => c.AccessUser).ToList();
+        }
+
+        public static void Exmpl02()
+        {
+            mcs db = new mcs();
+
+            //Загрузка одной вкладки
+
+            AccessTab tab = db.AccessTab.Where(w => w.intTabID == 1).FirstOrDefault();
+
+            addExmpl02(tab);
+
+            //Загрузка связанные данные с этой вкладки
+
+            // db.Entry(tab).Collection(c => c.AccessUser).Load();
+
+            //Console.WriteLine(tab.strTabName);
+            //foreach (var user in tab.AccessUser)
+            //{
+            //    Console.WriteLine("\t--> " +user.intUserId);
+            //}
+        }
+
+        private static void addExmpl02(AccessTab tab)
+        {
+            Console.WriteLine(tab.strTabName);
+            //foreach (var user in tab.AccessUser)
+            //{
+            //    Console.WriteLine("\t--> " + user.intUserId);
+            //}
+        }
+    }
+}
